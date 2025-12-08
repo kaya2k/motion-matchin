f4bb9ec47277b469afeb46c60969f42815bd68e0
SEUNGHO LEE <kayalee2k@gmail.com>
2025-12-09 07:26:40 +0900

    add motion_data.py

---
+++ core/motion_data.py
@@ -0,0 +1,27 @@
+import numpy as np
+from .bvh import BVH
+
+
+class MotionData:
+    """Class to handle motion capture data."""
+
+    n_frames: int
+    frame_time: float
+    n_joints: int
+    joints: list[str]
+    edge_list: list[tuple[int, int]]
+    positions: np.ndarray
+    rotations: np.ndarray
+
+    def __init__(self):
+        pass
+
+    def load_from_bvh(self, bvh_filename):
+        bvh = BVH(bvh_filename)
+        self.n_frames = bvh.n_frames
+        self.frame_time = bvh.frame_time
+        n_joints, edge_list, positions, rotations = bvh.get_motion_data()
+        self.n_joints = n_joints
+        self.edge_list = edge_list
+        self.positions = positions
+        self.rotations = rotations
