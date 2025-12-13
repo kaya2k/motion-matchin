c081cc4d47c4a10f990d6a5919f75c96a0e682a4
SEUNGHO LEE <kayalee2k@gmail.com>
2025-12-14 05:35:45 +0900

    complete motion matching wo smoothing

--- src/motion_matching/core/controller.py
+++ src/motion_matching/core/controller.py
@@ -17,7 +17,7 @@ class MotionMatchingController:
     """Class to control motion matching."""
 
     def __init__(self):
-        self.SEARCH_INTERVAL = 3
+        self.SEARCH_INTERVAL = 5
         self.LOCK_FOOT = False
 
         self.motion_dataset = []
@@ -26,20 +26,28 @@ class MotionMatchingController:
         self.feature_std = np.ones(FEATURE_DIM, dtype=np.float32)
         self.preprocess()
 
-        self.frame_after_search = 0
         self.input_direction = np.array([0.0, 0.0, 0.0])
         self.future_positions = np.zeros((len(OFFSETS), 3), dtype=np.float32)
         self.future_directions = np.zeros((len(OFFSETS), 3), dtype=np.float32)
 
-        self.origin_data_idx = 0
-        self.target_data_idx = 0
-        self.origin_frame = 0
+        self.target_data = 0
         self.target_frame = 0
+        self.frame_after_search = 1
 
         y = self.motion_dataset[0].positions[0, 0][1]
         self.root_position = np.array([0.0, y, 0.0])
         self.root_y_rotation = np.pi / 2.0
-        self.root_xz_speed = 0.0
+
+        self.root_velocity = np.array([0.0, 0.0, 0.0])
+        self.root_angular_velocity = 0.0
+
+        self.offset_position = np.array([0.0, 0.0, 0.0])
+        self.offset_velocity = np.array([0.0, 0.0, 0.0])
+        self.offset_y_rotation = 0.0
+        self.offset_angular_velocity = 0.0
+
+        self.origin_positions, self.origin_rotations = self.get_positions_rotations()
+        self.target_positions, self.target_rotations = self.get_positions_rotations()
 
     def preprocess(self):
         BVH_DIR = "./data/bvh"
@@ -64,33 +72,40 @@ class MotionMatchingController:
             self.feature_nns.append(nn)
 
     def update(self):
-        self.origin_frame += 1
+        if self.frame_after_search == self.SEARCH_INTERVAL:
+            self.frame_after_search = 1
+            self.origin_positions, self.origin_rotations = (
+                self.get_positions_rotations()
+            )
+            self.search()
+            self.target_positions, self.target_rotations = (
+                self.get_positions_rotations()
+            )
+
+        motion_data = self.motion_dataset[self.target_data]
+        root_R = R.from_euler("y", self.root_y_rotation)
+        translation = motion_data.translations[self.target_frame]
+        translation[1] = 0.0  # Ignore y translation
+        translation = root_R.apply(translation)
+        dy_rotation = motion_data.dy_rotations[self.target_frame]
+
+        # Update root position and y rotation
+        self.root_position += translation
+        self.root_velocity = translation
+        self.root_y_rotation = wrap_angle(self.root_y_rotation + dy_rotation)
+        self.root_angular_velocity = dy_rotation
+
+        self.update_future()
         self.target_frame += 1
         self.frame_after_search += 1
 
-        if self.frame_after_search == self.SEARCH_INTERVAL:
-            self.frame_after_search = 0
-            self.search()
-
-        motion_data = self.motion_dataset[self.origin_data_idx]
-
-        root_R = R.from_euler("y", self.root_y_rotation)
-        translation = motion_data.translations[self.origin_frame]
-        translation = root_R.apply(translation)
-
-        self.root_position += translation
-        self.root_y_rotation += motion_data.dy_rotations[self.origin_frame]
-        self.root_y_rotation = wrap_angle(self.root_y_rotation)
-        self.root_xz_speed = np.linalg.norm(project_to_xz(translation))
-
-        self.update_future()
-
     def update_future(self):
-        MAX_SPEED = 10.0
+        MAX_SPEED = 20.0
         ESTIMATED_TIME = 60
 
         root_R = R.from_euler("y", self.root_y_rotation)
-        local_root_velocity = np.array([-1.0, 0.0, 0.0]) * self.root_xz_speed
+        root_xz_speed = np.linalg.norm(project_to_xz(self.root_velocity))
+        local_root_velocity = np.array([-1.0, 0.0, 0.0]) * root_xz_speed
         local_input_direction = root_R.inv().apply(self.input_direction)
 
         target_position = local_input_direction * MAX_SPEED * ESTIMATED_TIME
@@ -102,14 +117,13 @@ class MotionMatchingController:
             direction = position1 - position0
             if np.linalg.norm(direction) > 1e-6:
                 direction /= np.linalg.norm(direction)
+            else:
+                direction = np.array([-1.0, 0.0, 0.0])
             self.future_positions[i] = position0
             self.future_directions[i] = direction
 
     def search(self):
         time_start = time.perf_counter()
-
-        self.origin_data_idx = self.target_data_idx
-        self.origin_frame = self.target_frame
 
         feature = self.get_current_feature()
         feature = self.normalize(feature)
@@ -129,40 +143,36 @@ class MotionMatchingController:
                 best_data_idx = data_idx
                 best_frame = frame
 
-        self.target_data_idx = best_data_idx
+        self.target_data = best_data_idx
         self.target_frame = best_frame
 
         time_end = time.perf_counter()
-        print(
-            (
-                f"\r[SEARCH] data {best_data_idx:1d} "
-                f"frame {best_frame:4d} "
-                f"distance {min_distance:.4f} "
-                f"time {time_end - time_start:.4f}s "
-                f"input {self.input_direction} "
-            ),
-            end="",
-        )
+        print(f"[SEARCH] data {best_data_idx:1d}", end=" ")
+        print(f"frame {best_frame:4d} ", end=" ")
+        print(f"distance {min_distance:.4f} ", end=" ")
+        print(f"time {time_end - time_start:.4f}s", end=" ")
+        print()
 
     def get_current_feature(self):
-        feature = np.zeros(FEATURE_DIM, dtype=np.float32)
+        feature = []
         for i in range(len(OFFSETS)):
-            feature[i * 4 : i * 4 + 2] = self.future_positions[i][[0, 2]]
-            feature[i * 4 + 2 : i * 4 + 4] = self.future_directions[i][[0, 2]]
+            feature.extend(self.future_positions[i][[0, 2]])
+            feature.extend(self.future_directions[i][[0, 2]])
 
-        motion_data = self.motion_dataset[self.origin_data_idx]
-        feature[12:] = motion_data.features[self.origin_frame, 12:]
-        return feature
+        root_R = R.from_euler("y", self.root_y_rotation)
+        feature.extend(root_R.inv().apply(self.root_velocity))
+
+        motion_data = self.motion_dataset[self.target_data]
+        feature.extend(motion_data.features[self.target_frame, -12:])
+        return np.array(feature)
 
     def normalize(self, feature):
         return (feature - self.feature_mean) / self.feature_std
 
-    def get_current_pose(self):
-        motion_data = self.motion_dataset[self.origin_data_idx]
-        joints = motion_data.joints
-        edges = motion_data.edges
-        positions = motion_data.positions[self.origin_frame]
-        rotations = motion_data.rotations[self.origin_frame]
+    def get_positions_rotations(self):
+        motion_data = self.motion_dataset[self.target_data]
+        positions = motion_data.positions[self.target_frame]
+        rotations = motion_data.rotations[self.target_frame]
 
         root_position = positions[0]
         root_y_rotation = extract_y_rotation(rotations[0])
@@ -170,10 +180,13 @@ class MotionMatchingController:
 
         positions = self.root_position + root_R.apply(positions - root_position)
         rotations = (root_R * R.from_euler("xyz", rotations)).as_euler("xyz")
+        return positions, rotations
 
+    def get_current_pose(self):
+        positions, rotations = self.get_positions_rotations()
         return (
-            joints,
-            edges,
+            self.motion_dataset[self.target_data].joints,
+            self.motion_dataset[self.target_data].edges,
             positions,
             rotations,
             self.future_positions,
